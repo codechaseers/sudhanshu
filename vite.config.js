935d0d2c93b5302818cc10d88fbb30d489baa578
sudhanshu shekhar ojha <sudhanshu@gmail.com>
2024-10-26 13:30:39 +0530

    seo meta tag vite-plugin-html  package add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        // Set your SEO metadata here
+        data: {
+          title: 'Sudhanshu Shekhar Ojha - Web Developer Portfolio', // Page title
+          description: 'Explore the portfolio of Sudhanshu Shekhar Ojha, showcasing projects, skills, and contact information for a professional web developer.', // Meta description
+          keywords: 'Sudhanshu Shekhar Ojha, Sudhanshu Ojha, web developer, portfolio, projects, skills, contact, frontend developer,Sudhanshu Protfolio,sudhansu,Sudhansu,Sudhansu Sekhar Ojha , bhadrak,Bhubaneswar,Sudhanshu Bhadrak,Sudhansu Bhubneswar ', // Keywords
+          ogUrl: 'https://sudhanshu-lyart.vercel.app/' // Your website URL
+        },
+      },
+    }),
+  ],
+});
